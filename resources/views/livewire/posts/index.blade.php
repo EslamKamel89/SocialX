644fe4893239c0214b64a12135931c1c76abb635
EslamKamel89 <elsamkamelforex@gmail.com>
2025-03-28 02:01:16 +0200

    update posts display in dashboard to include user information and avatars

--- resources/views/livewire/posts/index.blade.php
+++ resources/views/livewire/posts/index.blade.php
@@ -6,7 +6,7 @@ use App\Models\Post;
 new class extends Component {
     public function with(): array {
         return [
-            'posts' => Post::all(),
+            'posts' => Post::with(['user'])->get(),
         ];
     }
 }; ?>
@@ -15,21 +15,16 @@ new class extends Component {
     <div>
         <ul class="shadow-md list bg-base-100 rounded-box">
 
-            <li class="p-4 pb-2 text-xs tracking-wide opacity-60">Most played songs this week</li>
+            <li class="p-4 pb-2 text-xs tracking-wide opacity-60">All Posts</li>
+            @foreach ($posts as $post )
 
             <li class="list-row">
-                <div><img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/1@94.webp" /></div>
+                <div><img class="size-10 rounded-box" src="{{ $post->user->avatarUrl() }}" /></div>
                 <div>
-                    <div>Dio Lupa</div>
-                    <div class="text-xs font-semibold uppercase opacity-60">Remaining Reason</div>
+                    <div>{{ $post->user->name }}</div>
+                    <div class="text-xs font-semibold uppercase opacity-60">{{ $post->body }}</div>
                 </div>
-                <button class="btn btn-square btn-ghost">
-                    <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor">
-                            <path d="M6 3L20 12 6 21 6 3z"></path>
-                        </g>
-                    </svg>
-                </button>
+
                 <button class="btn btn-square btn-ghost">
                     <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
                         <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor">
@@ -39,49 +34,8 @@ new class extends Component {
                 </button>
             </li>
 
-            <li class="list-row">
-                <div><img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/4@94.webp" /></div>
-                <div>
-                    <div>Ellie Beilish</div>
-                    <div class="text-xs font-semibold uppercase opacity-60">Bears of a fever</div>
-                </div>
-                <button class="btn btn-square btn-ghost">
-                    <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor">
-                            <path d="M6 3L20 12 6 21 6 3z"></path>
-                        </g>
-                    </svg>
-                </button>
-                <button class="btn btn-square btn-ghost">
-                    <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor">
-                            <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
-                        </g>
-                    </svg>
-                </button>
-            </li>
+            @endforeach
 
-            <li class="list-row">
-                <div><img class="size-10 rounded-box" src="https://img.daisyui.com/images/profile/demo/3@94.webp" /></div>
-                <div>
-                    <div>Sabrino Gardener</div>
-                    <div class="text-xs font-semibold uppercase opacity-60">Cappuccino</div>
-                </div>
-                <button class="btn btn-square btn-ghost">
-                    <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor">
-                            <path d="M6 3L20 12 6 21 6 3z"></path>
-                        </g>
-                    </svg>
-                </button>
-                <button class="btn btn-square btn-ghost">
-                    <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                        <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor">
-                            <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
-                        </g>
-                    </svg>
-                </button>
-            </li>
 
         </ul>
     </div>
