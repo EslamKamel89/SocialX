3a4eb038880e2739d767718ef21856eeeb841380
EslamKamel89 <elsamkamelforex@gmail.com>
2025-04-03 21:46:56 +0200

    add heart icon component to post-item; implement conditional rendering for edit and delete buttons

--- resources/views/livewire/posts/post-item.blade.php
+++ resources/views/livewire/posts/post-item.blade.php
@@ -22,18 +22,18 @@ new class extends Component {
             <div class="text-xs font-semibold uppercase opacity-60">{{ $post->body }}</div>
         </div>
 
-        <button class="btn btn-square btn-ghost">
-            <svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                <g stroke-linejoin="round" stroke-linecap="round" stroke-width="2" fill="none" stroke="currentColor">
-                    <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z"></path>
-                </g>
-            </svg>
-        </button>
+        @can('update' , $post)
         <button class="btn btn-square btn-ghost" wire:click="edit">
             <x-icons.edit class="w-5 h-5 text-blue-900" />
         </button>
+        @endcan
+        @can('delete' , $post)
         <button class="btn btn-square btn-ghost" wire:confirm="Are you sure you want to delete this post?" wire:click="delete">
             <x-icons.trash class="w-5 h-5 text-red-900" />
+        </button>
+        @endcan
+        <button class="btn btn-square btn-ghost">
+            <x-icons.heart />
         </button>
     </li>
 
